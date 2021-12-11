cb5d01c561a440afb9b47de8470471d0dc0a4552
“cwpeng-cn” <cw.peng@foxmail.com>
2021-12-11 16:17:57 +0800

    add style images

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ net = TransferNet()
 net = restore_network("storage", "last", net)
 
 m_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((640, 480)),
     transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
 content_img = get_image("./images/test image.jpg", m_transform)
